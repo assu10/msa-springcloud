8ba2724906302170cdf4b98910664d63dfc4bf91
juhyun10 <juhyun1010@naver.com>
2020-08-08 16:08:20 +0900

    first

---
+++ src/main/java/com/assu/cloud/AssuParentPomApplication.java
@@ -0,0 +1,13 @@
+package com.assu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssuParentPomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssuParentPomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assu/cloud/AssuParentPomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assu.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssuParentPomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
