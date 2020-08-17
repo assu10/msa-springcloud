cbd031b45f28f090a103fd5997e2ea3fcd70dbf0
juhyun10 <juhyun1010@naver.com>
2020-08-17 18:50:58 +0900

    ..

--- eurekaserver/src/main/java/com/assu/cloud/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/assu/cloud/eurekaserver/EurekaserverApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaserverApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(EurekaserverApplication.class, args);
     }
