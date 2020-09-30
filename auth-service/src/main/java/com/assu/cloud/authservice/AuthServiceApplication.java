c6eeb07fd4238cb023f3b5fb55de73f0536336bb
assu10 <juhyun1010@naver.com>
2020-09-30 16:36:04 +0900

    jwt

--- auth-service/src/main/java/com/assu/cloud/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/assu/cloud/authservice/AuthServiceApplication.java
@@ -20,7 +20,7 @@ import java.util.Map;
 @EnableAuthorizationServer      // 이 서비스가 OAuth2 인증 서버가 될 것이라고 스프링 클라우드에 알림
 public class AuthServiceApplication {
     /**
-     * 사용자 정보 조회 시 사용
+     * 사용자 정보 조회 시 사용.
      *      OAuth2 로 보호되는 서비스에 접근하려고 할 때 사용
      *      보호 서비스로 호출되어 OAuth2 액세스 토큰의 유효성을 검증하고 보호 서비스에 접근하는 사용자 역할 조회
      */
