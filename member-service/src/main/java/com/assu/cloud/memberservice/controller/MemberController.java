f96dba758ce46a17237139d9bb9b86e57ed6e183
assu10 <juhyun1010@naver.com>
2020-11-01 23:59:03 +0900

    개별 회로 차단기를 사용자 정의하여 호출별 타임아웃 설정

--- member-service/src/main/java/com/assu/cloud/memberservice/controller/MemberController.java
+++ member-service/src/main/java/com/assu/cloud/memberservice/controller/MemberController.java
@@ -116,7 +116,6 @@ public class MemberController {
                                      value="5000")})   // 회로차단기의 타임아웃 시간을 5초로 설정*/
     @GetMapping(value = "timeout/{name}")
     public String timeout(ServletRequest req, @PathVariable("name") String name) {
-        logger.debug("LicenseService.getLicensesByOrg  Correlation id: {}", CustomContextHolder.getContext().getCorrelationId());
         return "[MEMBER] " + eventRestTemplateClient.gift(name) + " / port is " + req.getServerPort();
     }
 
